bad65fc47a923ae9c5295a458b6b487cc73f8ff6
Mehboob Ahmad <133740146+mehboob05@users.noreply.github.com>
2023-06-07 11:10:08 +0500

    update index js

---
+++ functions.js
@@ -0,0 +1,9 @@
+function sum(v1,v2){
+    console.log(v1+v2);
+}
+function mul(v1,v2){
+    console.log(v1*v2);
+}
+module.exports = {
+    sum,mul
+};
---
+++ index.js
@@ -0,0 +1,6 @@
+const {sum,mul} =require("./functions")
+
+
+
+sum(5,6);
+mul(5,7);
