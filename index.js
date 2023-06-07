5bdfe3c72f2ccdb232192b6ced471d359efb148b
Mehboob Ahmad <133740146+mehboob05@users.noreply.github.com>
2023-06-07 11:28:49 +0500

    os module function

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const {sum,mul} =require("./functions")
+// const {sum,mul} =require("./functions")
 
 
 
-sum(5,6);
-mul(5,7);
+// sum(5,6);
+// mul(5,7);
+
+// os moudles 
+
+const os =require("os");
+// console.log(os);
+
+// console.log(os.type);
+
+// console.log(os.cpus());
+
+// console.log(os.cpus().length);
+
+
