7debf287ddae36df9fb6ecb42381ef8749252d97
Mehboob Ahmad <133740146+mehboob05@users.noreply.github.com>
2023-06-12 11:56:32 +0500

    search query using params and query id

--- index.js
+++ index.js
@@ -10,12 +10,35 @@ app.get('/', (req, res) => {
         City: "Lodhran",
         Gender: "Male"
     }
-    res.json(student)
-   
-   
+    res.json(student); 
+
   })
+ 
+   //http://localhost:3004/search-fruit?id=2   data send using query
 
+   app.get("/search-fruits",(req,res)=>{
+    console.log(req.query.id);
 
+    const fruits =["apple","mango","orange","cherry","banana"];
+    const results = fruits[req.query.id]; 
+
+    res.json({
+      status:"ok",
+      results:results
+    });
+   })
+
+//http://localhost:3004/search/2    data send to sever using params
+app.get("/search/:id",(req,res)=>{
+  console.log(req.params.id);
+  const name=["mehboob","ali","raza","asif"];
+  const result = name[req.params.id];
+  res.json({
+status:"ok",
+ result:result
+
+  });
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
